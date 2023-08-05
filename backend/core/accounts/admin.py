2689f396752111d3283783d111e61a850d2ad63a
Rujikorn Khampho <jean@macbook-air.local>
2023-08-05 19:43:46 +0700

    feat: initial the user admin site

---
+++ backend/core/accounts/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from core.accounts.models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['email', 'first_name', 'last_name', 'is_staff']
+
+
+admin.site.register(User, UserAdmin)
